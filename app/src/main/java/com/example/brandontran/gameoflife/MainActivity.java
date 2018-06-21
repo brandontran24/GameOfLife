bd5fb82a42b4db45bbb51d0c1cccdb35f825a0ad
brandontran <brandontran4289@gmail.com>
2018-06-21 10:39:13 -0700

    initial commit

---
+++ app/src/main/java/com/example/brandontran/gameoflife/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.brandontran.gameoflife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.brandontran.gameoflife.Cell;
+
+public class MainActivity extends AppCompatActivity {
+
+    int test;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
